328a0ca850c5ce90408e974ef8c9772ac54d685b
mfortenberry <mfortenberry@yorksolutions.net>
2022-10-25 10:11:02 -0500

    comments w/ explainations

--- src/main/java/net/yorksolutions/storebackend/IndexController.java
+++ src/main/java/net/yorksolutions/storebackend/IndexController.java
@@ -2,7 +2,36 @@ package net.yorksolutions.storebackend;
 
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+// Problem in web applications: Client, and Server; each has its own state; need to keep the state in sync
+// state - data that can change
+// REST - well defined way for clients to retrieve and update a servers state
+//      - NOT JSON SERVER
+/*
+Server:
+State1: []
+
+State2: [{id: 5, name: madison}]
+
+Client:
+State1: []
+ */
+
+// Controller - maps client requests to methods
+// The thing that accepts http requests is not my application, it is TomCat
+// Spring will configure TomCat to call the methods that I specify using annotations when certain http requests are received
+
+// Request Life Cycle
+// 1. Client generates request
+// 2. TomCat receives the request
+// 3. TomCat routes the request to our application's method
+// 4. Our application's method returns a value
+// 5. Spring transforms the value into an HTTP response
+// 6. TomCat sends the response back to the Client
+
+// TomCat is HTTP server
+// HTTP Server is a program that listens for HTTP Requests and optionally responds
+
+@RestController // tells the program that this is a controller class, allows this class to use the REST functions
 @RequestMapping("/api") // localhost:8080/api
 public class IndexController {
     ProductService productService;
@@ -11,6 +40,11 @@ public class IndexController {
         this.productService = productService;
     }
 
+    // HTTP Request:
+    //   method: GET
+    //   url: http://localhost:8081/api/hello
+    //   headers?
+    //   body?
     @GetMapping("/hello")
     public String hello(){
         return "hello";
@@ -19,7 +53,11 @@ public class IndexController {
     public Iterable<Product> getProducts(){
         return this.productService.getAll();
     }
-    @PostMapping("/save")
+
+    // HTTP Request:
+    //   method: POST
+    //   url: http://localhost:8081/api/hello
+    @PostMapping("/hello")
     public void saveProduct(@RequestBody Product p){
         productService.save(p);
     }
--- src/main/java/net/yorksolutions/storebackend/StoreBackendApplication.java
+++ src/main/java/net/yorksolutions/storebackend/StoreBackendApplication.java
@@ -3,11 +3,30 @@ package net.yorksolutions.storebackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// decorators - annotation
+@SpringBootApplication // informs the application that we are using spring boot
 public class StoreBackendApplication {
 
+    // Object oriented programming - programming based around classes and objects, i guess
+    // Why use it?
+    //   makes thing scalable and reusable
+    //   keeps code organized
+    //   conducive to breaking down the problem into smaller problems
+    // problem - we want to create the same kind of object over and over
+    // class - template for an object
+    // problem - we have variable that needs to be associated with each other
+    // object - instance of a class
+    // object - group of fields/data/methods
+    // method is a function inside of a class
+    // static - can be called w/o an existing object
+
     public static void main(String[] args) {
+        // SpringApplication.run scan the given class
+        // Get the package that that class is defined in
+        // Look at all classes in that package for Spring Annotations
+        //
         SpringApplication.run(StoreBackendApplication.class, args);
     }
 
 }
+
